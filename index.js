720b92ec91b66a1fb159e5d2e2feabff4e3b2ec0
Matheus Goulart <matheus.goulart@ozmap.com.br>
2021-08-16 19:55:42 -0300

    Better commented code.

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+console.clear()
 console.log('\x1b[36m%s\x1b[0m','*R U N N I N G*')
 
 //Functions that generate data
 const generators = require("./src/functions/generators.js")
 //Test Values
+
+//Date
 console.log(generators.generateDate());
+//First name
 console.log(generators.generateFirstName())
+//Last name
 console.log(generators.generateLastName())
+//Full name
 console.log(generators.generateFullName())
+//Phone
 console.log(generators.generatePhone())
+//Phone with DDD
 console.log(generators.generatePhoneDDD())
+//Phone with DDD and DDI
 console.log(generators.generatePhoneDDI())
+//UUID
 console.log(generators.generateUUID())
+//email
